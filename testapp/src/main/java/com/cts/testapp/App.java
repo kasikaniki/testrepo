ef105f6056ed54be89758bc7354f221959f9d464
Velagala <421239@cognizant.com>
2017-06-03 17:58:35 -0500

    project added

---
+++ testapp/src/main/java/com/cts/testapp/App.java
@@ -0,0 +1,14 @@
+package com.cts.testapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        //test added test test test
+    }
+}
